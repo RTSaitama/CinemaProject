3268728dae26b76c2c1eec5c0277d72a4ae5fd70
“RTSaitama” <RTSaitama@outlook.com>
2023-11-23 04:24:37 +0200

    .

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react'; 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide,  } from 'swiper/react';
+ 
  import MovieCard from './MovieCard';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
+ 
 export default function HeroSlider(props){
 const [searchResults,setSearchResults] = React.useState(props.setSearchResults)
  
+
 const key =React.useId();
- 
+
 const list = props.setSearchResults.map(movie =>{
-  return  <SwiperSlide> <MovieCard item ={movie} key={key}/></SwiperSlide>
+ 
+  return  <SwiperSlide key={props.key} > <MovieCard  item ={movie}   
+   
+ /></SwiperSlide>
 })
   return (
     <Swiper  className='swiper'
@@ -18,17 +23,10 @@ const list = props.setSearchResults.map(movie =>{
       loop={true}
       effect={'coverflow'}
       grabCursor={true}
-      spaceBetween={18}
-      searchResults={searchResults}
-      coverflowEffect={{
-        rotate: 5,
-        stretch: 0,
-        depth: 30,
-        modifier: 2,
-        slideShadows: false,
-      }}
-      pagination={true}
-      modules={[EffectCoverflow, Pagination]}
+      spaceBetween={20}
+ 
+     
+ 
     >
        {list}
      </Swiper>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,59 @@
 import React from "react";
 import { SwiperSlide } from "swiper/react";
 import Swiper from "swiper";
+import NotFavoriteIcon from "./notfavourite";
+import FavouriteIcon from "./favourite";
+ 
 export default function MovieCard(props) {
- 
     const {Poster, Title, Year, imdbID}=props.item;
+
+    const [isFavourite,setIsFavourite] =React.useState(false);
+    const [favouritesList,setFavouritesList] = React.useState('');
+    const [favIcon,setFavIcon] = React.useState(<NotFavoriteIcon/>)
+
+
+    React.useEffect(()=> 
+    {ifFavouriteCheck()},[]);
+    React.useEffect(()=> isFavourite ?setFavIcon(<FavouriteIcon/>) : setFavIcon(<NotFavoriteIcon/>),[isFavourite]);
+  const ifFavouriteCheck =() =>{
+    const favouritesList = localStorage.getItem('favouritesList') || '';
+    setIsFavourite(favouritesList.includes(imdbID));
+    setFavouritesList(favouritesList);
+  }
+    const handleToFavorite = () =>{
+    const favouritesList = localStorage.getItem('favouritesList') || '';
+    const separator = favouritesList.length ? ',' : '';
+    if(!isFavourite) {
+      localStorage.setItem('favouritesList', favouritesList + separator +imdbID);
+      setIsFavourite(true)
+    }  }
+  
+ const fullPlotRequest = ()=>{
+    const url = `http://www.omdbapi.com/?i=${imdbID}&plot=full&apikey=5bd71ded&`;
+     (fetch(url).then(response=> response.json()))
+  };
  
+
+
+
     return(
     
-        <div className="swiper-slide">
-                {Poster ? <img src={Poster} className='movieCardPoster' alt="moviePoster"/>:null}
-                <h3 className="movie_card_title">{Title || 'N/A'}</h3>
+        <div className="swiper-slide"  >
+             <div className="toFavorites_container" onClick={handleToFavorite}  data-id={imdbID}  imdbID={imdbID} setIsFavourite={setIsFavourite}>
+ 
+     
+     {favIcon}
+ 
+    
+
+  </div >   
+                {Poster ? <img src={Poster} className='movieCardPoster' alt="moviePoster" /> :null}
+                <div className="movie_card_desc">
                 <p className="movie_card_year">{Year}</p>
-                <a href='#' data-id={imdbID}>Details</a>
+                <h3 className="movie_card_title">{Title || 'N/A'}</h3>
+                <a className="movie_card_details" href='#' data-id={imdbID} onClick={fullPlotRequest}> </a>
+              
+                </div>
         </div>
    
     )
